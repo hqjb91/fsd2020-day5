006978a6e369594a1f77c5e0e1c7e0b66b35e060
He Quanjie Boey <hqjb91@gmail.com>
2020-10-30 13:20:05 +0800

    Added caching and remember previous values

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ require('dotenv').config();
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000;
 
 // Set up variables
-const countries = ['us', 'gb', 'jp', 'sg' , 'fr', 'cn'];
-const categories = ['Business', 'Entertainment', 'General', 'Health', 'Science', 'Sports', 'Technology'];
+const countries = [{country:'us',checked:''}, {country:'gb',checked:''}, {country:'jp',checked:''}, {country:'sg' ,checked:''}, {country:'fr',checked:''}, {country:'cn', checked:''}];
+const categories = [{category:'Business',selected:''}, {category:'Entertainment',selected:''}, {category:'General',selected:''}, {category:'Health',selected:''}, {category:'Science',selected:''}, {category:'Sports',selected:''}, {category:'Technology',selected:''}];
 const cache = [];
 
 // Create an instance of express
@@ -28,6 +28,9 @@ app.use(express.static(__dirname + '/static'));
 // Configure the routes
 app.get('/', (req, res) => {
 
+    categories.forEach(e => e.selected=''); // Reset remembered categories
+    countries.forEach(e => e.checked=''); // Reset remembered country
+
     res.status(200);
     res.type('text/html');
     res.render('index', {
@@ -37,37 +40,54 @@ app.get('/', (req, res) => {
 
 app.post('/', express.urlencoded({extended:true}), async (req, res) => {
 
-    const searchTerms = {};
-    const endpoint = 'https://newsapi.org/v2/top-headlines'
-    const url = withQuery(endpoint, {
-        q: req.body.newsSearch,
-        apiKey: process.env.API_KEY,
-        country: req.body.countries,
-        category: req.body.categories.toLowerCase(),
-    });
+    const searchTerms = {q, country, category} = req.body;
+    const timeNow = new Date();
+    const matchedArticles = cache.filter(e=>{
+        return (JSON.stringify(e.searchTerms) === JSON.stringify(searchTerms)) && 
+        (((timeNow.getTime() - e.date.getTime())/(1000 * 3600 * 24)) < 1);
+    }); // Check if not expired and matched
 
-    const results = await fetch(url);
-    const resultsJSON = await results.json();
-    
-    const articles = resultsJSON.articles.map((article) => {
-        return { 
-            title: article.title,
-            urlToImage: article. urlToImage,
-            description: article.description,
-            publishedAt: article.publishedAt,
-            url: article.url }
-    });
+    categories.forEach(e => {
+        if(e.category === category) {e.selected='selected'} else {e.selected=''};
+    }); // Remember previous category
 
-    // Store in cache
-    cache.push({})
+    countries.forEach(e => {
+        if(e.country === country) {e.checked='checked'} else {e.checked=''};
+    }); // Remember previous country
+
+    let articles = [];
+
+    // Check if cached and not expired and load from cache else fetch from API and push into cache
+    if(matchedArticles.length > 0) {
+        console.log('Searched before and articles cached are returned');
+        articles = matchedArticles[0].articles;
+    } else {
+        const endpoint = 'https://newsapi.org/v2/top-headlines';
+        const url = withQuery(endpoint, { q, apiKey: process.env.API_KEY, country, category });
+
+        const results = await fetch(url);
+        const resultsJSON = await results.json();
+        
+        articles = resultsJSON.articles.map((article) => {
+            return { 
+                title: article.title,
+                urlToImage: article. urlToImage,
+                description: article.description,
+                publishedAt: article.publishedAt,
+                url: article.url }
+        });
+
+        // Store in cache
+        cache.push({searchTerms,articles, date: timeNow});
+    }
 
     res.status(200);
     res.type('text/html');
-    res.render('index', {countries, categories, articles});
+    res.render('index', {searchTerms, countries, categories, articles});
 
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`You are connected on port : ${PORT} at ${new Date()}.`)
-})
+    console.log(`You are connected on port : ${PORT} at ${new Date()}.`);
+});
